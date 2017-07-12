e1af94bbda9f5dccc73b7131a1ebd1ba349c94c1
Evgeniy <vasenev.ea@mail.ru>
2017-07-12 10:53:19 +0300

    Created

---
+++ src/MinecraftServerAPI/MinecraftServerAPI/MinecraftServerAPI/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftServerAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = loadConfig("config.txt");
+
+            StreamWriter inputStream = null;
+            StreamReader outputStream = null;
+
+            startMinecraftServer(conf.path,conf.args, ref inputStream, ref outputStream);
+
+            //
+            while (!outputStream.EndOfStream)
+            {
+                string line = outputStream.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+
+
+
+        private static void startMinecraftServer(string path,string args, ref StreamWriter inputStream, ref StreamReader outputStream)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            inputStream = proc.StandardInput;
+            outputStream = proc.StandardOutput;
+        }
+
+        private static Config loadConfig(string path)
+        {
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+        }
+
+        class Config
+        {
+            public string path { get; set; }
+            public string args { get; set; }
+        }
+    }
+}
